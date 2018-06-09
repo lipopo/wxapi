76899d73fa2f4aa5326220e1597c8b82d25db702
lipo <15510520668@163.com>
2018-06-09 09:16:28 +0800

    first commit

---
+++ WechatAPI/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import HttpResponse
+import hashlib
+
+# Create your views here.
+def AccessServer(request):
+    if request.method == 'GET':
+        echoStr = request.GET.get('echostr')
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+        signature = request.GET.get('signature')
+        token = 'tttLZPppap'
+        access = [timestamp, nonce, token]
+        linkStr = reduce(lambda x, y: x + y, access.sort(), '')
+        hashCode = hashlib.sha1(linkStr)
+        print hashCode
+        print signature
+        if hashCode == signature:
+            return HttpResponse(echoStr)
