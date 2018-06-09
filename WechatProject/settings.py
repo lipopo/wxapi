17ef8999a41cc783431d516a70cb866d43a74ee6
lipo <15510520668@163.com>
2018-06-09 10:46:47 +0800

    修复版本BUG

--- WechatProject/settings.py
+++ WechatProject/settings.py
@@ -16,7 +16,6 @@ import os
 import djcelery
 djcelery.setup_loader()
 
-from celery.schedules import crontab
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
